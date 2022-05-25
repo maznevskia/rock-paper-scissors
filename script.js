b9d34a96929a145a9f8911f261f6e86d8b585b31
maznevskia <andrej.maznevski13@gmail.com>
2022-05-25 16:28:05 +0200

    Plan and Plain Text Setup

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// computerPlay function that will randomly return 'Rock', 'Paper' or 'Scissors'
+
+// function that plays a single round of Rock Paper Scissors
+// takes 2 parameters playerSelection and computerSelection
+// returns a string that declares the winner of the round 
+// ex. 'You lose! Paper beats Rock'
+// make playerSelection case-sensitive
+
+// function game that calls playRound function 5 times
+// keeps score and reports a winner or loser at the end
+// console.log() the winner
