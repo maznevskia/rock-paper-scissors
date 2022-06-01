c63ad073644f17d7f31c1cfa8d2afb4e4e925ad7
maznevskia <andrej.maznevski13@gmail.com>
2022-06-01 13:57:40 +0200

    Comment and code cleanup

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// computerPlay function that will randomly return 'Rock', 'Paper' or 'Scissors'
-
-// function that plays a single round of Rock Paper Scissors
-// takes 2 parameters playerSelection and computerSelection
-// returns a string that declares the winner of the round 
-// ex. 'You lose! Paper beats Rock'
-// make playerSelection case-insensitive
-
-// function game that calls playRound function 5 times
-// keeps score and reports a winner or loser at the end
-// console.log() the winner
-
 function computerPlay(){
-    // return random computer pick
-
     let picks = ['Rock', 'Paper', 'Scissors'];
     let randomPick = picks[Math.floor(Math.random() * picks.length)];
 
@@ -20,8 +6,6 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection = computerPlay()){
-    // game logic if player chooses Scissors
-    
     if ( playerSelection == 'Scissors' ) {
         if ( computerSelection == 'Rock' ) {
             console.log('You Lose! Rock beats Scissors');
@@ -40,8 +24,6 @@ function playRound(playerSelection, computerSelection = computerPlay()){
     }
     
     else if ( playerSelection == 'Paper' ) {
-        // game logic if player chooses Paper
-        
         if ( computerSelection == 'Rock' ) {
             console.log('You Win! Rock beats Paper');
             return 1;
@@ -59,8 +41,6 @@ function playRound(playerSelection, computerSelection = computerPlay()){
     }
     
     else if ( playerSelection == 'Rock' ) {
-        // game logic if player chooses Rock
-
         if ( computerSelection == 'Scissors' ) {
             console.log('You Win! Rock beats Scissors');
             return 1;
@@ -75,12 +55,6 @@ function playRound(playerSelection, computerSelection = computerPlay()){
             console.log( 'Tie!' );
             return 0;
         }
-    }
-
-    // if players input is invalid
-
-    else {
-        console.log( 'Typo!' );
     }
 }
 
@@ -103,36 +77,19 @@ const btnPlay = (choice) => {
         score2 = 0;
         div.append(p);
     }
-
-    
 };
 
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
 
-let score1 = 0, score2 = 0;
-
-btnRock.addEventListener('click', () => {
-    btnPlay('Rock');
-});
-
-btnPaper.addEventListener('click', () => {
-    btnPlay('Paper');
-});
-
-btnScissors.addEventListener('click', () => {
-    btnPlay('Scissors');
-});
-
 const div = document.querySelector('div');
-
 const p = document.createElement('p');
 
-// click of button starts play round
+let score1 = 0, score2 = 0;
 
-// in play round we change the result which will be displayed on screen
+btnRock.addEventListener('click', () => btnPlay('Rock'));
 
-// in the event listener function we will change the 
+btnPaper.addEventListener('click', () => btnPlay('Paper'));
 
-// lastly the score will be updated
+btnScissors.addEventListener('click', () => btnPlay('Scissors'));
